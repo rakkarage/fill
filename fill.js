0361d05264544e03ea47a7e1d0d1af1fb62de93e
Rakka Rage <rakkarage@gmail.com>
2019-05-02 21:16:16 -0400

    x

--- fill.js
+++ fill.js
@@ -1,11 +1,18 @@
 document.getElementById("addButton").addEventListener("click", function (event) {
 	var li = document.createElement("li");
-	li.textContent = "test";
+	li.textContent = uuidv4();
 	document.getElementById("list").appendChild(li);
 	updateScroll();
 });
 
 function updateScroll() {
-	var element = document.getElementById("container");
+	var element = document.getElementById("scroll");
 	element.scrollTop = element.scrollHeight;
+}
+
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
 }
